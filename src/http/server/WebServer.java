c657cea07e2253667f3dce32562a56dd0dc113b8
Benjamin Kermani <benjamin.kermani@gmail.com>
2020-10-20 10:09:22 +0200

    WebServer: Added Javadoc comments

--- src/http/server/WebServer.java
+++ src/http/server/WebServer.java
@@ -1,4 +1,7 @@
-///A Simple Web Server (WebServer.java)
+/**
+ * HTTP server implementing GET, PUT, POST, DELETE and HEAD requests
+ * @author GALERZA Javier, BEL Corentin, KERMANI Benjamin
+ */
 
 package http.server;
 
@@ -9,7 +12,6 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 import static java.lang.Integer.parseInt;
 
@@ -28,7 +30,8 @@ public class WebServer {
 
 
     /**
-     * WebServer constructor.
+     * WebServer constructor
+     * @param port , port number on which ServerSocket will be created
      */
     protected void start(int port) {
         ServerSocket s;
@@ -52,25 +55,10 @@ public class WebServer {
                 // remote is now the connected socket
                 System.out.println("Connection, sending data.");
                 //Opening input/output binary stream of client socket
-
                 BufferedInputStream in_bis = new BufferedInputStream(remote.getInputStream());
                 BufferedOutputStream out = new BufferedOutputStream(remote.getOutputStream());
-                // read the data sent. We basically ignore it,
-                // stop reading once a blank line is hit. This
-                // blank line signals the end of the client HTTP
-                // headers.
                 // Header must finish with CR LF CR LF sequence (e.q \r\n\r\n in java)
                 String header = "";
-                String str = ".";
-
-                /*
-                 * Condition to check header (MUST BE COMPLETED)
-                 *  warning: when using postman or executing the 1st time a request to the server on a web browser
-                 *  Client will do 2 requests:
-                 * - favicon : returns null
-                 * - URL : returns GET /URL HTTP....
-                 * THIS LOOP DOESN'T FETCH DATA THAT MAY BE CONTAIN IN BODY's REQUEST
-                 */
                 boolean right_syntax = false;
                 while (in_bis.available() > 0) {
                     int cur = in_bis.read();
@@ -80,19 +68,17 @@ public class WebServer {
                         break;
                     }
                 }
+                System.out.println("REQUEST: \n" + header);
                 if (right_syntax) {
-                    System.out.println("REQUEST: \n" + header);
                     String[] header_decomposed = header.split(" ");
                     String http_method = header_decomposed[0];
 
                     if (!HTTP_METHODS.contains(http_method)) {
                         out.write(buildResponseHeader("501", "Not Implemented").getBytes());
-                        out.flush();
-                        remote.close();
                     } else {
                         String resource = header_decomposed[1];
-                        resource = resource.substring(1); // To get rid of '/' character
-                        System.out.println(http_method + " " + resource);
+                        // To get rid of '/' character
+                        resource = resource.substring(1);
                         if (EMPTY_RESSOURCE.equals(resource)) {
                             resource = INDEX_PATH;
                         }
@@ -117,29 +103,46 @@ public class WebServer {
                                     break;
                             }
                         }
-                        out.flush();
-                        remote.close();
                     }
+                    out.flush();
+                    remote.close();
                 } else {
-                    System.out.println("REQUEST: \n" + header);
                     out.write(buildResponseHeader("400", "Bad Request").getBytes());
                     out.flush();
                     out.close();
                 }
             } catch (Exception e) {
+                e.printStackTrace();
                 System.out.println("Error: " + e);
             }
         }
     }
 
 
+    /**
+     * Build simple response header that will be returned to client
+     * @param status HTTP code status
+     * @param message message explaining HTTP code status
+     * @return response header as string
+     */
     protected String buildResponseHeader(String status, String message) {
         String header = "HTTP/1.0 " + status + ' ' + message + "\r\n";
         header += "Server: Bot\r\n";
         header += "\r\n";
+        System.out.println(header);
         return header;
     }
 
+
+    /**
+     * Build complex response header that will be returned to client,
+     * allowing him to interpret response depending on content-type
+     * @param status HTTP code status
+     * @param message message explaining HTTP code status
+     * @param filename filename to find corresponding extension (file type)
+     * @param length file length (in bytes)
+     * @return response header as string
+     */
     protected String buildResponseHeader(String status, String message, String filename, long length) {
         System.out.println("REPONSE : ");
         String header = "HTTP/1.0 " + status + ' ' + message + "\r\n";
@@ -168,13 +171,12 @@ public class WebServer {
     }
 
     /**
-     * Method that returns resource
+     * HTTP method that returns resource to client, allowing him to visualize the resource he asked for. (
      * filename doesn't exist -> 404 Not found
      * otherwise -> 200 OK
      * exception -> 500 Internal error
-     *
-     * @param out,      output flux to client socket
-     * @param filename, filepath
+     * @param out output flux to client socket
+     * @param filename filepath
      */
     protected void httpGET(BufferedOutputStream out, String filename) {
         try {
@@ -199,16 +201,16 @@ public class WebServer {
     }
 
     /**
-     * Method to create a resource (or replace an existing one).
-     * If the file is new, creates it using data from request's body message.
-     * If the file is old, replaces it using data from request's body message.
+     * HTTP method that creates a resource (or replace an existing one).
+     * If file doesn't exist, creates it using data from request's body message.
+     * If file is old, replaces it (i.e, erase previous data) using data from request's body message.
      * File is old -> 200, OK
      * File is new -> 201, Created
      * exception -> 500, Internal error
      *
-     * @param in,       client socket input stream, to read body message
-     * @param out,      client socket output stream, to write response
-     * @param filename, filepath
+     * @param in client socket input stream, to read body message
+     * @param out client socket output stream, to write response
+     * @param filename filepath
      */
     protected void httpPUT(BufferedInputStream in, BufferedOutputStream out, String filename) {
         try {
@@ -217,9 +219,6 @@ public class WebServer {
             PrintWriter pw = new PrintWriter(file);
             pw.close();
             BufferedOutputStream fileOut = new BufferedOutputStream(new FileOutputStream(file));
-            /**
-             * @// TODO: 18/10/2020  doesn't entirely work
-             */
             byte[] buffer = new byte[256];
             while (in.available() > 0) {
                 int nbRead = in.read(buffer);
@@ -243,24 +242,21 @@ public class WebServer {
         }
     }
 
+
     /**
-     * Method to replace an existing resource (or create one)
-     * If file exists -> add at the end request's body message -> 200, OK
-     * If file is new -> create a new file -> 201, Created
+     * HTTP method that replaces an existing resource (or creates one )
+     * If file exists -> append at the end of file's data request's body message -> 200, OK
+     * If file doesn't exist-> creates a new file -> 201, Created
      * exception -> 500, Internal error
-     *
-     * @param in,       client socket input stream, to read body message
-     * @param out,      client socket output stream, to write response
-     * @param filename, filepath
+     * @param in client socket input stream, to read body message
+     * @param out client socket output stream, to write response
+     * @param filename filepath
      */
     protected void httpPOST(BufferedInputStream in, BufferedOutputStream out, String filename) {
         try {
             File file = new File(filename);
             boolean has_existed = file.exists();
             BufferedOutputStream fileOut = new BufferedOutputStream(new FileOutputStream(file, has_existed));
-            /**
-             * @// TODO: 18/10/2020  doesn't entirely work
-             */
             byte[] buffer = new byte[256];
             while (in.available() > 0) {
                 int nbRead = in.read(buffer);
@@ -284,14 +280,14 @@ public class WebServer {
         }
     }
 
+
     /**
-     * Method that delete a file given its filepath.
+     * HTTP method that deletes a file given its filepath.
      * If deleted -> 204, No Content
      * If can't delete -> 403, Forbidden
      * If not found -> 404, Not Found
      * exception -> 500, Internal error
-     *
-     * @param out,      client socket output stream, to return header
+     * @param out, client socket output stream, to return header
      * @param filename, filepath
      */
     protected void httpDELETE(BufferedOutputStream out, String filename) {
@@ -318,10 +314,10 @@ public class WebServer {
         }
     }
 
+
     /**
-     * Method that verify the existence of a resource.
-     *
-     * @param out,      client socket output flux, to send header
+     * HTTP method returns headers that would be returned if specified resource was requested with HTTP GET.
+     * @param out, client socket output flux, to send header
      * @param filename, filepath
      */
     protected void httpHEAD(BufferedOutputStream out, String filename) {
@@ -343,10 +339,10 @@ public class WebServer {
         }
     }
 
+
     /**
      * Start the application.
-     *
-     * @param args Command line parameters are not used.
+     * @param args, port number
      */
     public static void main(String[] args) {
         if (args.length != 1) {
